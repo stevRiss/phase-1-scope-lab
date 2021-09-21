680c59e9b0b8ca09bf7f2684713282b27d6f209d
Stephen <stevriss22@gmail.com>
2021-09-21 18:13:16 -0400

    commit tree 46d5b1b07fa8a2d7c53c9587a66136ef96674697

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+
+function fixScope(){
+
+    return customerName;
+}
+////console.log(fixScope(customerName));
+
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    //return upperName.toUpperCase();
+    return customerName;
+}
+//console.log(upperCaseCustomerName(customerName)); 
+
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+//console.log(setBestCustomer())
+
+function overwriteBestCustomer(overwrite){
+    overwrite = 'maybe bob';
+    bestCustomer = overwrite;
+    return bestCustomer;
+}
+//console.log(overwriteBestCustomer());
+
+const leastFavoriteCustomer = "betty";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob';
+    return leastFavoriteCustomer;
+
+}
+//console.log(changeLeastFavoriteCustomer());
+
+
+//win for today;
+//struggle for today
+//most beautiful place iv been 
